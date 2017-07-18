3a4db1b175f028e642f68f91f7f2c660c7408b62
Venkat Peri <venkatperi@gmail.com>
2017-07-18 13:15:41 -0400

    initial

---
+++ src/main/java/org/antlr4/v4/gui/DefaultTreeTextProvider.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
+package org.antlr.v4.gui2;
+
+import org.abego.treelayout.NodeExtentProvider;
+import org.abego.treelayout.TreeForTreeLayout;
+import org.abego.treelayout.TreeLayout;
+import org.abego.treelayout.util.DefaultConfiguration;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.Utils;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.Tree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.Trees;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.RuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.atn.ATN;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.misc.Predicate;
+import org.antlr.v4.runtime.misc.Utils;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.prefs.Preferences;
+
+
+public class DefaultTreeTextProvider implements TreeTextProvider {
+  private final List<String> ruleNames;
+  private final Parser parser;
+
+  //public DefaultTreeTextProvider(List<String> ruleNames) {
+    //this.ruleNames = ruleNames;
+  //}
+
+  public DefaultTreeTextProvider(Parser recog) {
+    this.parser = recog;
+    String[] ruleNames = recog != null ? recog.getRuleNames() : null;
+    this.ruleNames= ruleNames != null ? Arrays.asList(ruleNames) : null;
+  }
+
+  @Override
+  public String getText(Tree node) {
+    return String.valueOf(getNodeText(node));
+  }
+
+	public String getNodeText(Tree t) {
+		if ( ruleNames!=null ) {
+			if ( t instanceof RuleContext ) {
+				int ruleIndex = ((RuleContext)t).getRuleContext().getRuleIndex();
+				String ruleName = ruleNames.get(ruleIndex);
+				int altNumber = ((RuleContext) t).getAltNumber();
+				if ( altNumber!=ATN.INVALID_ALT_NUMBER ) {
+					return ruleName+":"+altNumber;
+				}
+				return ruleName;
+			}
+			else if ( t instanceof ErrorNode) {
+				return t.toString();
+			}
+			else if ( t instanceof TerminalNode) {
+				Token symbol = ((TerminalNode)t).getSymbol();
+				if (symbol != null) {
+          String type = parser.getVocabulary().getSymbolicName(symbol.getType());
+					String s = symbol.getText();
+					return "[" + type + "] " + s;
+				}
+			}
+		}
+		// no recog for rule names
+		Object payload = t.getPayload();
+		if ( payload instanceof Token ) {
+			return ((Token)payload).getText();
+		}
+		return t.getPayload().toString();
+	}
+}
